edd7ece6137085bad355b1643d0e1b062ea069b8
jisooyu <jisooyu@hotmail.com>
2023-06-22 18:29:16 +0900

    changing the height of the Paper

--- components/PostPrayer.js
+++ components/PostPrayer.js
@@ -1,46 +1,68 @@
-'use client'
+'use client';
 import { Grid, Typography, Paper, useMediaQuery } from '@mui/material';
 
 const PostPrayer = ({ postPrayer }) => {
-    const isIphone13 = useMediaQuery('(max-width: 400px)');
+	const isIphone13 = useMediaQuery('(max-width: 400px)');
 
-    const paperStyle = {
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        marginTop: isIphone13 ? '1em' : '3em',
-        marginLeft: isIphone13 ? '1em' : '5em',
-        height: isIphone13 ? '100vh' : '100vh',
-        width: isIphone13 ? '80vw' : '50vw',
-        paddingTop:  '1em' ,
-        paddingLeft:'1em' ,
-        paddingRight: '2em' ,
-        overflow: 'scroll'
-    };
+	const paperStyle = {
+		display: 'flex',
+		flexDirection: 'column',
+		alignItems: 'center',
+		justifyContent: 'center',
+		marginTop: isIphone13 ? '1em' : '3em',
+		marginLeft: isIphone13 ? '1em' : '5em',
+		height: isIphone13 ? '100vh' : '100vh',
+		width: isIphone13 ? '80vw' : '50vw',
+		paddingTop: '1em',
+		paddingLeft: '1em',
+		paddingRight: '2em',
+		overflow: 'scroll',
+	};
 
-
-    return (
-        <Paper elevation={0} sx={paperStyle}>
-            <Grid container spacing={0.5}>
-                <Grid item xs={12} sm={3} textAlign="center" >
-                    <Typography variant="h5" component="div">
-                        {postPrayer.title}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={6} textAlign="left" >
-                    <Typography variant="subtitle1">
-                        {postPrayer.content}
-                    </Typography>
-                </Grid>
-                <Grid item xs={12} sm={3} textAlign="center" >
-                    <Typography variant="h6">
-                        {postPrayer.ending}
-                    </Typography>
-                </Grid>
-            </Grid>
-        </Paper>
-    );
+	return (
+		<Paper
+			elevation={0}
+			sx={paperStyle}
+			style={{
+				height: '400px',
+			}}
+		>
+			<Grid
+				container
+				spacing={0.5}
+			>
+				<Grid
+					item
+					xs={12}
+					sm={3}
+					textAlign='center'
+				>
+					<Typography
+						variant='h5'
+						component='div'
+					>
+						{postPrayer.title}
+					</Typography>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={6}
+					textAlign='left'
+				>
+					<Typography variant='subtitle1'>{postPrayer.content}</Typography>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm={3}
+					textAlign='center'
+				>
+					<Typography variant='h6'>{postPrayer.ending}</Typography>
+				</Grid>
+			</Grid>
+		</Paper>
+	);
 };
 
 export default PostPrayer;
--- components/Prayer.js
+++ components/Prayer.js
@@ -1,53 +1,92 @@
-'use client'
-import { styled, Grid, Paper, Typography, ButtonBase, useMediaQuery } from '@mui/material'
+'use client';
+import {
+	styled,
+	Grid,
+	Paper,
+	Typography,
+	ButtonBase,
+	useMediaQuery,
+} from '@mui/material';
 
 const Prayer = ({ prayer }) => {
-    const isIphone13 = useMediaQuery('(max-width: 200px)');
-    const Img = styled('img')({
-        margin: 'auto',
-        display: 'block',
-        maxWidth: '100%',
-        maxHeight: '100%',
-    });
-    const paperStyle = {
-        height: isIphone13 ? '80vh' : '100vh',
-        width: isIphone13 ? '80vw' : '65vw',
-        paddingTop:  '1em',
-        paddingLeft: '1em',
-        marginLeft:  '1em'
-    };
+	const isIphone13 = useMediaQuery('(max-width: 200px)');
+	const Img = styled('img')({
+		margin: 'auto',
+		display: 'block',
+		maxWidth: '100%',
+		maxHeight: '100%',
+	});
+	const paperStyle = {
+		height: isIphone13 ? '80vh' : '100vh',
+		width: isIphone13 ? '80vw' : '65vw',
+		paddingTop: '1em',
+		paddingLeft: '1em',
+		marginLeft: '1em',
+	};
 
-    return (
-        <Paper
-            elevation={0}
-            sx={paperStyle}
-        >
-            <Grid container spacing={2}>
-                <Grid item>
-                    <ButtonBase sx={{ width: 150, height: 150 }}>
-                        <Img alt="complex" src={prayer.img} />
-                    </ButtonBase>
-                </Grid>
-                <Grid item xs={12} sm container>
-                    <Grid item xs container direction="column" spacing={2}>
-                        <Grid item xs>
-                            <Typography gutterBottom variant="subtitle1" component="div">
-                                {prayer.title}
-                            </Typography>
-                            <Typography variant="body2" gutterBottom>
-                                {prayer.content}
-                            </Typography>
-                        </Grid>
-                        <Grid item>
-                            <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                                {prayer.repeat}
-                            </Typography>
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Paper>
-
-    );
-}
+	return (
+		<Paper
+			elevation={0}
+			sx={paperStyle}
+			style={{
+				height: '200px',
+			}}
+		>
+			<Grid
+				container
+				spacing={2}
+			>
+				<Grid item>
+					<ButtonBase sx={{ width: 150, height: 150 }}>
+						<Img
+							alt='complex'
+							src={prayer.img}
+						/>
+					</ButtonBase>
+				</Grid>
+				<Grid
+					item
+					xs={12}
+					sm
+					container
+				>
+					<Grid
+						item
+						xs
+						container
+						direction='column'
+						spacing={2}
+					>
+						<Grid
+							item
+							xs
+						>
+							<Typography
+								gutterBottom
+								variant='subtitle1'
+								component='div'
+							>
+								{prayer.title}
+							</Typography>
+							<Typography
+								variant='body2'
+								gutterBottom
+							>
+								{prayer.content}
+							</Typography>
+						</Grid>
+						<Grid item>
+							<Typography
+								sx={{ cursor: 'pointer' }}
+								variant='body2'
+							>
+								{prayer.repeat}
+							</Typography>
+						</Grid>
+					</Grid>
+				</Grid>
+			</Grid>
+		</Paper>
+	);
+};
 export default Prayer;
