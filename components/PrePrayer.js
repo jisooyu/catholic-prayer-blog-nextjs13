a7a8833c226d2274ca87dcf0ef5bdd0183363fd9
jisooyu <jisooyu@hotmail.com>
2023-07-16 15:49:59 +0900

    setting paddingTop to 10px

--- components/PrePrayer.js
+++ components/PrePrayer.js
@@ -7,7 +7,7 @@ const PrePrayer = ({ prePrayer }) => {
 	const paperStyle = {
 		height: isIphone13 ? '140vh' : '115vh',
 		width: isIphone13 ? '90vw' : '60vw',
-		paddingTop: isIphone13 ? '2em' : '0.5em',
+		paddingTop: isIphone13 ? '2em' : '10px',
 		paddingLeft: isIphone13 ? '2em' : '1em',
 		marginLeft: isIphone13 ? '1em' : '1em',
 		marginRight: isIphone13 ? '3em' : '1em',
